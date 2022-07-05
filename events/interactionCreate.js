6c3ad727456c0e267aeb29cb96ab9df0e035f63a
comcom3 <comcom3@comcom3ui-MacBookPro.local>
2022-07-05 17:50:23 +0900

    separate "event module" & "command module" (checkpoint)

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,32 @@
-// module.exports = {
-// 	name: 'interactionCreate',
-// 	execute(interaction) {
-//         if (!interaction.isCommand()) return;
+module.exports = {
+	name: 'interactionCreate',
+	async execute(interaction) {
+        if (!interaction.isCommand()) return;
+    
+        const command = interaction.client.commands.get(interaction.commandName);
+    
+        if (!command) return;
+    
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+        }
+    },
+};
 
-//         const command = client.commands.get(interaction.commandName);
-    
-//         if (!command) return;
-    
-//         try {
-//             await command.execute(interaction);
-//         } catch (error) {
-//             console.error(error);
-//             await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-//         }
-// 	},
-// };
+// client.on('interactionCreate', async interaction => {
+// 	if (!interaction.isCommand()) return;
+
+// 	const command = client.commands.get(interaction.commandName);
+
+// 	if (!command) return;
+
+// 	try {
+// 		await command.execute(interaction);
+// 	} catch (error) {
+// 		console.error(error);
+// 		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+// 	}
+// });
